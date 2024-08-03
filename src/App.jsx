3f5134a2484a130258433af9c45880e163f708af
Cole <colehartman00@gmail.com>
2024-08-03 15:46:07 -0700

    font setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+//import { useState } from "react";
+import { motion } from "framer-motion"
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="">Hello</div>
+      <motion.div
+        className="box text-center"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.5,
+          ease: [0, 0.71, 0.2, 1.01]
+        }}>Project Pilot</motion.div>
     </>
   );
 }
