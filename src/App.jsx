bdd1485f63ad35817d62b2e0a795b078553e26c8
Cole <colehartman00@gmail.com>
2024-08-04 13:05:41 -0700

    feat - dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 //import { useState } from "react";
-import { motion } from "framer-motion"
-import "./App.css";
+import { motion } from "framer-motion";
+import Paper from '@mui/material/Paper';
 
 function App() {
   return (
     <>
       <motion.div
-        className="box text-center"
+        className="box text-center text-2xl"
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{
@@ -14,6 +14,7 @@ function App() {
           delay: 0.5,
           ease: [0, 0.71, 0.2, 1.01]
         }}>Project Pilot</motion.div>
+      <Paper elevation={4}>TEST</Paper>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,43 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import App from './App.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const router = createBrowserRouter([
-
   {
     path: "/",
     element: <App />
   }
 ])
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+function Root() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
