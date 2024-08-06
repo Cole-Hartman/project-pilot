b1b3f93e2bb20323f1460a550b3bca989bdaa1c5
Cole <colehartman00@gmail.com>
2024-08-05 19:11:42 -0700

    feat - building home page

--- src/App.jsx
+++ src/App.jsx
@@ -37,13 +37,17 @@ function App() {
               duration: 1.7,
               bounce: 0.3
             }}>
-            <div className="text-lg px-2 mt-3">
-              <div className="mb-2">Find Personalized Project Ideas to Elevate Your Skills and Portfolio.</div>
-              <div>Project Pilot will tailor it's recommendations against your current</div>
-              <div className="flex justify-center gap-2">
-                <div className="text-blue-500">skill level,</div>
-                <div>and</div>
-                <div className="text-blue-500"> ambition.</div>
+            <div className="text-lg px-2 mt-3 flex flex-col">
+              <div>
+                <div className="mb-2 inline">Find Personalized Project Ideas tailored against your current </div>
+                <div className="text-blue-500 inline">skill level, </div>
+                <div className="inline">and</div>
+                <div className="text-blue-500 inline"> ambition.</div>
+              </div>
+
+              <div className="pt-3">
+                <div className="inline">Simplified for </div>
+                <div className="inline text-blue-500">optimal AI comprehension.</div>
               </div>
             </div>
           </motion.div>
@@ -69,10 +73,78 @@ function App() {
             ></video>
           </div>
 
-          <div className="mx-3 text-5xl font-bold mt-20">
-            <div ref={aboutRef} className="mb-5">WORKS AROUND YOU</div>
+          <div className="text-5xl font-bold mt-10 pt-10 mx-5" ref={aboutRef}>WORKS AROUND YOU</div>
+          <div className="mt-2 mx-2 text-center">Project Pilot is a tool for effortlessly generating tailored and industry standard computer science project ideas. </div>
+          <div className="mt-2 mx-2 text-center mb-5">Designed for students, hobbyists, and professionals alike, our app transforms the way you brainstorm and plan your next big project.</div>
+
+          <div className="ml-5 mr-4">
             <img src={MobileTree} alt="" />
           </div>
+
+          <motion.div
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true, amount: 0.4 }} // Trigger animation once when 20% of the element is visible
+            transition={{
+              type: 'spring',
+              stiffness: 50,
+              damping: 10,
+              duration: 2,
+              bounce: 0.2
+            }}
+          >
+            <Paper className="mx-2 mt-3 px-3 py-3">
+              <ul className="text-left mt-2">
+                <li className="font-bold">Effortless Idea Generation:</li>
+                <li className="pl-3">Skip the hassle of typing out every detail about your project needs. With just a few clicks, you can generate highly specific and relevant project ideas tailored to your experience level and interests.</li>
+              </ul>
+            </Paper>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, x: [-50] }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true, amount: 0.4 }} // Trigger animation once when 20% of the element is visible
+            transition={{
+              type: 'spring',
+              stiffness: 50,
+              damping: 10,
+              duration: 2,
+              bounce: 0.2
+            }}
+          >
+            <Paper className="mx-2 mt-3 px-3 py-3">
+              <ul className="text-left mt-2">
+                <li className="font-bold">Save Time and Effort:</li>
+                <li className="pl-3">Focus on bringing your ideas to life rather than struggling with brainstorming. Our app takes care of the ideation process so you can spend more time on what matters most—building and creating.</li>
+              </ul>
+            </Paper>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true, amount: 0.4 }} // Trigger animation once when 20% of the element is visible
+            transition={{
+              type: 'spring',
+              stiffness: 50,
+              damping: 10,
+              duration: 2,
+              bounce: 0.2
+            }}
+          >
+            <Paper className="mx-2 mt-3 px-3 py-3">
+              <ul className="text-left mt-2">
+                <li className="font-bold">Industry-Relevant Insights:</li>
+                <li className="pl-3">Stay ahead of the curve with project ideas that incorporate current tech job market trends and industry standards. We use up-to-date data to ensure your projects are not only innovative but also aligned with what’s relevant and sought after in the tech world.</li>
+              </ul>
+            </Paper>
+          </motion.div>
+
+          <div className="mx-3 text-5xl font-bold mt-20">
+            <div className="mb-5 pt-5">Q&A</div>
+          </div>
+
 
           <motion.div
             initial={{ opacity: 0 }}
